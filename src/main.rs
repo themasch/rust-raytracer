802235a2fa94f5df2619a1fbd8f50a843f73935d
Mark Schmale <masch@masch.it>
2019-08-03 19:43:42 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -38,17 +38,9 @@ fn main() {
         .unwrap_or(&String::from("0"))
         .parse()
         .unwrap_or(45.0);
+    println!("rendering with {:?}° rot.", idx);
     let rotation = Deg(idx * 2.0);
 
-    /*let str = format!(r#"
-        v -2.0 0.0  2.0  # 1
-        v  2.0 0.0  2.0  # 2
-        v  0.0 {} -2.0  # 3
-
-        f 1 2 3
-    "#, y);*/
-    //let simple =  String::from(str);
-    //let teapot_read = wavefront_obj::obj::parse(simple);
     let teapot_read = wavefront_obj::obj::parse(String::from(include_str!("../teapot.obj")));
 
     if let Err(err) = teapot_read {
@@ -114,12 +106,12 @@ fn main() {
         )
         .add_object(
             ObjectBuilder::create_for(Mesh::create(object.clone()))
-                /*.with_material(Material::reflective_color(
+                .with_material(Material::reflective_color(
                     Color::from_rgb(0.6, 0.6, 0.6),
                     0.2,
-                    0.2
-                ))*/
-                .with_material(Material::diffuse_color(Color::from_rgb(0.5, 0.5, 0.5), 0.2))
+                    0.02
+                ))
+                //.with_material(Material::diffuse_color(Color::from_rgb(0.5, 0.5, 0.5), 0.2))
                 .scale(1.0)
                 .rotation(Quaternion::one() + Quaternion::from_angle_y(rotation))
                 .at_position(Point::new(0.0, -2.0, -6.0))
@@ -156,56 +148,9 @@ fn main() {
         Ok(_) => {}
     };
 
-    /*println!(
+    println!(
         "render: {:?}, save: {:?}",
         format_time(&before_save.duration_since(before_render)),
         format_time(&before_save.elapsed())
-    );*/
+    );
 }
-
-/*
-#[cfg(test)]
-#[macro_use]
-extern crate assert_approx_eq;
-
-#[cfg(test)]
-use raycast::Ray;
-#[cfg(test)]
-use image::{DynamicImage, GenericImage};
-
-#[test]
-fn test_can_render_scene() {
-    let scene = SceneBuilder::new(1920, 1080)
-        .add_object(Object::Sphere(Sphere {
-            center: Point::new(0.0, 0.0, -5.0),
-            radius: 1.0,
-            material: Material::diffuse_color(Color::from_rgb(1.0, 0.1, 0.1), 0.2)
-        }))
-        .finish();
-    let s_w = scene.width;
-    let s_h = scene.height;
-    let img: DynamicImage = render(scene);
-    assert_eq!(s_w, img.width());
-    assert_eq!(s_h, img.height());
-}
-
-
-#[test]
-fn test_creates_prime_ray() {
-    let scene = SceneBuilder::new(640, 480)
-        .add_object(Object::Sphere(Sphere {
-            center: Point::new(0.0, 0.0, -5.0),
-            radius: 1.0,
-            material: Material::diffuse_color(Color::from_rgb(1.0, 0.1, 0.1), 0.2)
-        }))
-        .finish();
-
-    let ray = Ray::create_prime(20, 20, &scene);
-
-    assert_eq!(0.0, ray.origin.x);
-    assert_eq!(0.0, ray.origin.y);
-    assert_eq!(0.0, ray.origin.z);
-    assert_approx_eq!(1.0, ray.direction.magnitude());
-}
-
-*/
